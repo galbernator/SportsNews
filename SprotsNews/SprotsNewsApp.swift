c7583dde60d3a6cc39deff5cbb202ab9c886fe7e
Steve Galbraith <steve@stevegalbraith.com>
2020-11-13 19:04:19 -0700

    Initial Commit

---
+++ SprotsNews/SprotsNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SprotsNewsApp.swift
+//  SprotsNews
+//
+//  Created by Steve Galbraith on 11/13/20.
+//
+
+import SwiftUI
+
+@main
+struct SprotsNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
